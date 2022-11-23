7a7c8cfe49e9f347b38b5c6430212d1c180c9db9
Gerard Baholli <gerardbaholli@live.it>
2022-11-23 19:43:05 +0100

    Add first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Ball : MonoBehaviour
+{
+    
+	[SerializeField] float anchorMoveDuration = 0.5f;
+    
+	private Vector3 mOffset;
+	private float mZCoord;
+    
+	private void Start()
+    {
+	    //this.transform.position = AnchorPosition(this.transform.position);
+	    transform.DOMove(AnchorPosition(this.transform.position), anchorMoveDuration);
+    }
+    
+	private Vector2 AnchorPosition(Vector2 pos)
+	{
+		int newX;
+		int newY;
+		int x = (int) pos.x;
+		int y = (int) pos.y;
+		
+		if (x + 0.5 >= pos.x)
+			newX = x;
+		else
+			newX = x + 1;
+			
+		if (y + 0.5 >= pos.y)
+			newY = y;
+		else
+			newY = y + 1;
+			
+		Debug.Log("[" + newX + ", " + newY + "]");
+		
+		return new Vector2(newX, newY);
+	}
+
+
+	public Vector2 GetPosition()
+	{
+		return AnchorPosition(this.transform.position);
+    }
+
+	
+	private void OnMouseDown()
+	{
+		mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+		// Store offset = gameobject world pos - mouse world pos
+		mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+
+        
+	}
+
+	private Vector3 GetMouseAsWorldPoint()
+	{
+		// Pixel coordinates of mouse (x,y)
+		Vector3 mousePoint = Input.mousePosition;
+
+		// z coordinate of game object on screen
+		mousePoint.z = mZCoord;
+
+		// Convert it to world points
+		return Camera.main.ScreenToWorldPoint(mousePoint);
+	}
+
+	private void OnMouseDrag()
+	{
+		transform.position = GetMouseAsWorldPoint() + mOffset;     
+	}
+
+	private void OnMouseUp()
+	{
+        transform.DOMove(AnchorPosition(this.transform.position), anchorMoveDuration);
+    }
+  
+}
---
+++ Assets/Scripts/PositionValidator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionValidator : MonoBehaviour
+{
+
+    [SerializeField] bool debugOption = false;
+    [SerializeField] Ball ball;
+
+    private void Start()
+    {
+        IsValidPosition(ball.transform.position);
+    }
+
+
+    public bool IsValidPosition(Vector2 pos)
+    {
+        Debug.Log(pos.x + " " + pos.y);
+
+        if ((0 <= pos.x && pos.x <= 2) && (0 <= pos.y && pos.y <= 2))
+        {
+            if (debugOption)
+                Debug.Log("Invalid position: Q1");
+            return false;
+        }
+
+        if ((6 <= pos.x && pos.x <= 8) && (0 <= pos.y && pos.y <= 2))
+        {
+            if (debugOption)
+                Debug.Log("Invalid position: Q3");
+            return false;
+        }
+
+        if ((0 <= pos.x && pos.x <= 2) && (6 <= pos.y && pos.y <= 8))
+        {
+            if (debugOption)
+                Debug.Log("Invalid position: Q7");
+            return false;
+        }
+
+        if ((6 <= pos.x && pos.x <= 8) && (6 <= pos.y && pos.y <= 8))
+        {
+            if (debugOption)
+                Debug.Log("Invalid position: Q9");
+            return false;
+        }
+
+        if (debugOption)
+            Debug.Log("Valid position: " + pos.x + " " + pos.y);
+        return true;
+    }
+
+
+}
